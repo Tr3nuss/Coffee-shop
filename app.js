3b824f511a339543f41355b15af32beb30a5557b
Ivan Pronin <pacernestit@rambler.ru>
2024-02-09 23:31:06 +0300

    8: add burger menu and other adaptive

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const burger = document.querySelector(".burger-menu");
+const menuItems = document.querySelector(".second-header-items");
+
+burger.addEventListener("click", () => {
+  menuItems.classList.toggle("show-menu");
+});
+
 function Counter() {
   location.href = "cart.html";
 }
