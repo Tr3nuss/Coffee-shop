4e1b6b8369bbe78e63f8d08d82d7bd615d06f101
Ivan Pronin <pacernestit@rambler.ru>
2024-04-05 22:52:34 +0300

    master fix (добавил мобильный хедер)

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const burger = document.querySelector(".burger-menu");
-const menuItems = document.querySelector(".second-header-items");
+const menuItems = document.querySelector(".burger-menu");
 
 burger.addEventListener("click", () => {
   menuItems.classList.toggle("show-menu");
